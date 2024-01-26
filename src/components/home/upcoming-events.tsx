93264e2a8985184e95e06669ad1c200bd47c95e6
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2024-01-26 15:21:24 +0200

    fetching with gql

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,13 +1,45 @@
 import { useState } from "react";
 import { CalendarOutlined } from "@ant-design/icons";
 import { Badge, Card, List } from "antd";
+import { useList } from "@refinedev/core";
 
 import { Text } from "../text";
 import UpcomingEventsSkeleton from "../skeleton/upcoming-events";
 import { getDate } from "@/utils/helpers";
+import { DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY } from "@/graphql/queries";
 
 const UpcomingEvents = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const { data, isLoading } = useList({
+    resource: "events",
+    meta: {
+      gqlQuery: DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY,
+    },
+    // queryOptions: {
+    //   enabled: !isLoading,
+    //   filters: [
+    //     {
+    //       field: "date",
+    //       operator: "gte",
+    //       value: getDate(new Date()),
+    //     },
+    //   ],
+    //   sort: [
+    //     {
+    //       field: "date",
+    //       order: "asc",
+    //     },
+    //   ],
+    // },
+    // pagination: { pageSize: 5 },
+    // onFinish: () => {
+    //   setIsLoading(false);
+    // },
+  });
+
+  console.log("data", data);
+
   return (
     <Card
       style={{ height: "100%" }}
@@ -39,7 +71,7 @@ const UpcomingEvents = () => {
       ) : (
         <List
           itemLayout="horizontal"
-          dataSource={[]}
+          dataSource={data?.data || []}
           renderItem={(item) => {
             const renderDate = getDate(item.startDate, item.endDate);
             return (
