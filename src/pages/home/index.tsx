229d62189723f23c56e51aed2a6c97f763383556
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2024-01-27 18:19:37 +0200

    total count card with chart

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,51 @@
-import DealsChart from "@/components/home/deals-chart";
-import UpcomingEvents from "@/components/home/upcoming-events";
+import { useCustom } from "@refinedev/core";
 import { Col, Row } from "antd";
 
+import DealsChart from "@/components/home/deals-chart";
+import DashboardTotalCountCard from "@/components/home/total-count-card";
+import UpcomingEvents from "@/components/home/upcoming-events";
+import { DASHBOARD_TOTAL_COUNTS_QUERY } from "@/graphql/queries";
+import { DashboardTotalCountsQuery } from "@/graphql/types";
+
 export const Home = () => {
+  const { data, isLoading } = useCustom<DashboardTotalCountsQuery>({
+    url: "",
+    method: "get",
+    meta: {
+      gqlQuery: DASHBOARD_TOTAL_COUNTS_QUERY,
+    },
+  });
+
   return (
     <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource="companies"
+            isLoading={isLoading}
+            totalCount={data?.data.companies.totalCount || 0}
+          />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource="contacts"
+            isLoading={isLoading}
+            totalCount={data?.data.contacts.totalCount || 0}
+          />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard
+            resource="deals"
+            isLoading={isLoading}
+            totalCount={data?.data.deals.totalCount || 0}
+          />
+        </Col>
+      </Row>
       <Row gutter={[32, 32]} style={{ marginTop: "32px" }}>
         <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
           <UpcomingEvents />
         </Col>
-        <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
+        <Col xs={24} sm={24} xl={16} style={{ height: "460px" }}>
           <DealsChart />
         </Col>
       </Row>
